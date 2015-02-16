05592bd669f16d76c92d4f3d3b622f42aef437fa
Tirso Peralta <tirso.peralta@gmail.com>
2015-02-16 18:44:03 -0500

    Test if mc is an instance of MortgageCalculator Class

--- lib/mortgage_calculator.rb
+++ lib/mortgage_calculator.rb
@@ -1,3 +1,9 @@
 class MortgageCalculator
 
+  def initialize(args)
+    @principal = args[:principal]
+    @interest_rate = args[:interest_rate]
+    @mortgage_period = args[:mortgage_period]
+  end
+
 end
--- test/mortgage_calculator_test.rb
+++ test/mortgage_calculator_test.rb
@@ -1,6 +1,13 @@
 require './test_helper'
 require './lib/mortgage_calculator'
 
-class MortageCalculatorTest < Minitest::Test
+class MortgageCalculatorTest < Minitest::Test
 
+  def setup
+    @mc = MortgageCalculator.new(principal: 100000, interest_rate: 4, mortgage_period: 30)
+  end
+
+  def test_mc_instance
+    assert_instance_of MortgageCalculator, @mc
+  end
 end
